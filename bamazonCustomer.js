72df8ebece5a5e02d9a80a2562e8b9c1fe78d756
Pamela Vasquez <pamelavasquez94@gmail.com>
2020-02-01 23:06:33 -0500

    DOCS: added message to notify users of purchase success

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -16,6 +16,16 @@ var connection = mysql.createConnection({
   database: "bamazon_db"
 });
 
+var items = [
+  [
+    "\033[1;97;41mItem Id\033[00m",
+    "\033[1;97;41mProduct Name\033[00m",
+    "\033[1;97;41mDepartment\033[00m",
+    "\033[1;97;41mPrice\033[00m",
+    "\033[1;97;41mQuantity\033[00m"
+  ]
+];
+
 inquirer
   .prompt([
     {
@@ -37,15 +47,6 @@ inquirer
 
 function queryAllInventory() {
   connection.query("SELECT * FROM products", function(err, res) {
-    var items = [
-      [
-        "\033[1;97;41mItem Id\033[00m",
-        "\033[1;97;41mProduct Name\033[00m",
-        "\033[1;97;41mDepartment\033[00m",
-        "\033[1;97;41mPrice\033[00m",
-        "\033[1;97;41mQuantity\033[00m"
-      ]
-    ];
     if (err) throw err;
     for (var i = 0; i < res.length; i++) {
       items.push([
@@ -65,29 +66,41 @@ function queryAllInventory() {
 }
 
 function buySomething() {
-  inquirer.prompt([
-    {
-      type: "list",
-      name: "choice",
-      message: "Select the item you are interested in purchasing.",
-      choices: [
-        "Baby Yoda Funko Pop!",
-        "Playstation 5",
-        "Rumba Vacuum Cleaner",
-        "Hydro Flask Water Bottle",
-        "Michael Jackson Red Leather",
-        "LEGO: Star Wars",
-        "Gordon Ramsey Cookbook",
-        "Aveeno Daily Face Wash",
-        "Yankee Candle Large - Bahama Breeze",
-        "Orbit Gum - Spearmint (Pack of 5)"
-      ]
-    },
-    {
-      type: "number",
-      name: "quantity",
-      message:
-        "How many units of this product are you interested in purchasing?"
-    }
-  ]);
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "choice",
+        message: "Select the item you are interested in purchasing.",
+        choices: [
+          { value: 1, name: "Baby Yoda Funko Pop!" },
+          { value: 2, name: "Playstation 5" },
+          { value: 3, name: "Rumba Vacuum Cleaner" },
+          { value: 4, name: "Hydro Flask Water Bottle" },
+          { value: 5, name: "Michael Jackson Red Leather" },
+          { value: 6, name: "LEGO: Star Wars" },
+          { value: 7, name: "Gordon Ramsey Cookbook" },
+          { value: 8, name: "Aveeno Daily Face Wash" },
+          { value: 9, name: "Yankee Candle Large - Bahama Breeze" },
+          { value: 10, name: "Orbit Gum - Spearmint (Pack of 5)" }
+        ]
+      },
+      {
+        type: "number",
+        name: "quantity",
+        message:
+          "How many units of this product are you interested in purchasing?"
+      }
+    ])
+    .then(answers => {
+      if (answers.quantity <= items[answers.choice][4]) {
+        console.log(
+          `It's YOURS. You're total cost is $${items[answers.choice][3] *
+            answers.quantity} \n\nOnce your payment is process, we will ship your item(s) out. Thank you for shopping with us!`
+        );
+      } else
+        console.log(
+          "Currently we do not have enough quantity of this product to fulfill your order. Please check again at another time for restock. \n \nWe apologize for the inconveninece. :("
+        );
+    });
 }
