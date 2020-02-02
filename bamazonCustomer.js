c66a909ca21e394e09c257d927bd22ac4e1f6ac8
Pamela Vasquez <pamelavasquez94@gmail.com>
2020-02-01 20:58:52 -0500

    DOC: mysql database configured

---
+++ bamazonCustomer.js
@@ -0,0 +1,42 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "",
+  database: "bamazon_db"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  queryAllInventory();
+});
+
+function queryAllInventory() {
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+    for (var i = 0; i < res.length; i++) {
+      console.log(
+        res[i].item_id +
+          " | " +
+          res[i].product_name +
+          " | " +
+          res[i].department_name +
+          " | " +
+          res[i].price +
+          " | " +
+          res[i].stock_quantity
+      );
+    }
+    console.log("-----------------------------------");
+  });
+}
