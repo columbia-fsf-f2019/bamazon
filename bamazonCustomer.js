c9372acf3fe38cc54773338b9851aa5db4526a7d
Pamela Vasquez <pamelavasquez94@gmail.com>
2020-02-01 22:35:10 -0500

    DOCS: added prompt for user and database data in table form for terminal

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+const { table } = require("table");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -15,28 +16,78 @@ var connection = mysql.createConnection({
   database: "bamazon_db"
 });
 
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId);
-  queryAllInventory();
-});
+inquirer
+  .prompt([
+    {
+      type: "confirm",
+      name: "welcome",
+      message: "Welcome to Bamazon! Are you ready to shop?"
+    }
+  ])
+  .then(answers => {
+    if (answers.welcome) {
+      connection.connect(function(err) {
+        if (err) throw err;
+        console.log("Below is a list of our available invetory");
+        queryAllInventory();
+      });
+    } else
+      console.log("We hope to see you again when you are ready to shop :)");
+  });
 
 function queryAllInventory() {
   connection.query("SELECT * FROM products", function(err, res) {
+    var items = [
+      [
+        "\033[1;97;41mItem Id\033[00m",
+        "\033[1;97;41mProduct Name\033[00m",
+        "\033[1;97;41mDepartment\033[00m",
+        "\033[1;97;41mPrice\033[00m",
+        "\033[1;97;41mQuantity\033[00m"
+      ]
+    ];
     if (err) throw err;
     for (var i = 0; i < res.length; i++) {
-      console.log(
-        res[i].item_id +
-          " | " +
-          res[i].product_name +
-          " | " +
-          res[i].department_name +
-          " | " +
-          res[i].price +
-          " | " +
-          res[i].stock_quantity
-      );
+      items.push([
+        res[i].item_id,
+        res[i].product_name,
+        res[i].department_name,
+        res[i].price,
+        res[i].stock_quantity
+      ]);
     }
-    console.log("-----------------------------------");
+    let data, output;
+    data = items;
+    output = table(data);
+    console.log(output);
+    buySomething();
   });
+}
+
+function buySomething() {
+  inquirer.prompt([
+    {
+      type: "list",
+      name: "choice",
+      message: "Select the item you are interested in purchasing.",
+      choices: [
+        "Baby Yoda Funko Pop!",
+        "Playstation 5",
+        "Rumba Vacuum Cleaner",
+        "Hydro Flask Water Bottle",
+        "Michael Jackson Red Leather",
+        "LEGO: Star Wars",
+        "Gordon Ramsey Cookbook",
+        "Aveeno Daily Face Wash",
+        "Yankee Candle Large - Bahama Breeze",
+        "Orbit Gum - Spearmint (Pack of 5)"
+      ]
+    },
+    {
+      type: "number",
+      name: "quantity",
+      message:
+        "How many units of this product are you interested in purchasing?"
+    }
+  ]);
 }
